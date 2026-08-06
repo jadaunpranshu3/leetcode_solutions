6511636ab0b3a4267404f9fc0c866d015d1aaf4b
Pranshu Jadon <jadaunpranshu3@gmail.com>
2026-08-06 23:42:16 +0530

    Time: 0 ms (100%), Space: 13.4 MB (6.11%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def search(self, nums, target):
+        n=len(nums)
+        low=0
+        high=n-1
+        while low<=high:
+            mid=(low+high)//2
+            if target==nums[mid]:
+                return mid
+            elif target<nums[mid]:
+                high=mid-1
+            else:
+                low=mid+1
+        return -1
+        
